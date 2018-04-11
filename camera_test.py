518f3d933357492803255eda0dbcd9ade63fb08d
royjan <38300518+royjan@users.noreply.github.com>
2018-04-12 01:19:20 +0300

    Add files via upload
    camera test

---
+++ camera_test.py
@@ -0,0 +1,22 @@
+import cv2
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    cap.set(3, 1920)
+    cap.set(4, 1080)
+    # cv.set(cv.CV_CAP_PROP_FRAME_HEIGHT, 600)
+
+    # Our operations on the frame come here
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    frame = horizontal_img = cv2.flip(frame, 1)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
